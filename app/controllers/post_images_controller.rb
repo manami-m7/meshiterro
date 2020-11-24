7f971f4e186284b6fc881df60bb7c47e78289580
manami-m7 <kmuuum@gmail.com>
2020-11-24 13:35:59 +0000

    [update4-3]

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,18 +1,19 @@
 class PostImagesController < ApplicationController
-  
+
   def new
+    @post_image = PostImage.new
   end
-  
+
   def create
   end
-  
+
   def index
   end
 
   def show
   end
-  
+
   def destroy
   end
-  
+
 end
