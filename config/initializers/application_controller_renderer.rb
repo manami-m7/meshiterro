7c51993d7320f297b8dfc388ced745e4c300a921
manami-m7 <kmuuum@gmail.com>
2020-11-25 02:59:15 +0000

    [Update4-3]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'fb2e2aaa5e0a13ec90472f680386a09c92a6b5778811834f5f2230efc3812ad0313c038a2b6b210767823ab2092917f0299a85da60a50f55a413416e72fc2bc3'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :post_imaages, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
