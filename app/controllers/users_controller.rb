e100b3023782b3102e18f3cc14ecc5fe6c3d50dc
manami-m7 <kmuuum@gmail.com>
2020-11-26 08:24:17 +0000

    [Finish4-5]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.finnd(params[:id])
+    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
